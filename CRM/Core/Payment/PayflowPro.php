fa9dc45094fa5f1e12a9a1a6b197b81033cda0cc
Richard Baugh <richard@graydigitalgroup.com>
2024-01-31 10:15:03 -0600

    Update checkConfig message

--- CRM/Core/Payment/PayflowPro.php
+++ CRM/Core/Payment/PayflowPro.php
@@ -64,7 +64,7 @@ class CRM_Core_Payment_PayflowPro extends CRM_Core_Payment {
   public function checkConfig() {
     $errorMsg = [];
     if (empty($this->_paymentProcessor['user_name'])) {
-      $errorMsg[] = ' ' . ts('ssl_merchant_id is not set for this payment processor');
+      $errorMsg[] = ' ' . ts('Vendor ID is not set for this payment processor');
     }
 
     if (empty($this->_paymentProcessor['url_site'])) {
