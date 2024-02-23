58b995637106e0dc3ba899b1891583965ffc3aee
Matthew Wire <mjw@mjwconsult.co.uk>
2024-02-23 11:41:15 +0000

    Fix job params

--- managed/ImportLatestRecurPaymentsJob.mgd.php
+++ managed/ImportLatestRecurPaymentsJob.mgd.php
@@ -9,16 +9,16 @@ return [
     'cleanup' => 'always',
     'update' => 'unmodified',
     'params' => [
-      'version' => 4,
       'values' => [
+        'is_active' => FALSE,
         'name' => 'PayflowPro: Import latest recur Payments',
         'description' => E::ts('Get the latest payments using PayflowPro API for each recurring payment and record/update in CiviCRM.'),
         'run_frequency' => 'Daily',
         'api_entity' => 'PayflowPro',
         'api_action' => 'importLatestRecurPayments',
-        'parameters' => '',
+        'parameters' => 'version=4
+paymentProcessorID=1',
       ],
-      'is_active' => FALSE,
       'match' => [
         'name',
       ],
